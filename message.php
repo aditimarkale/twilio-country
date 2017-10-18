f7f6546408dc085700fcf7c3141d6387bfa7eb32
Aditi Markale <aditimarkale1@gmail.com>
2017-10-17 23:47:41 -0400

    Test16

--- message.php
+++ message.php
@@ -4,6 +4,9 @@ $body = $_POST['Body'];
 $url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
 $file = file_get_contents($url);
 $data = json_decode($file, true);
+use Twilio\Twiml;
+
+$response = new Twiml();
     foreach ($data as $character) {  
         if($character['country'] == $body) {
           $city = $character['city'];
